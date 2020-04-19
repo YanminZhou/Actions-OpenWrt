033adebea92e099ab13e90ee65d5e456b68e1cd7
zhouym233 <zhouym233@foxmail.com>
2020-04-19 14:28:20 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,4 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#git clone https://github.com/fw876/helloworld
+sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
